6f732c6dc3c166fa7d90cea02449adc553ab2ddb
sarukiti <sarukiti1891@gmail.com>
2025-01-19 13:40:16 +0900

    USBと通信する部分

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,26 +1,55 @@
+use motor_lib::{blmd, device_type, md, sd, smd, USBHandle};
+use tauri::{Manager, State};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn motor_pwm(address: u8, power: i16) {
-    println!("アドレス0x{:>02x}のモーターが{power}で回ったとしましょう", address);
+fn motor_pwm(handle: State<'_, USBHandle>, address: u8, power: i16) {
+    let result = md::send_pwm(&*handle, address | device_type::MD, power).unwrap();
+    println!("{:?}", result);
 }
 #[tauri::command]
-fn solenoid_state(address: u8, port: u8, state: i16) {
-    println!("アドレス0x{:>02x}のSDのポート{port}の出力が{state}になったとしましょう", address | 0x10);
+fn solenoid_state(handle: State<'_, USBHandle>, address: u8, port: u8, state: i16) {
+    let result = sd::send_power(&*handle, address | device_type::SD, port, state).unwrap();
+    println!("{:?}", result);
 }
 #[tauri::command]
-fn servo_angle(address: u8, port: u8, angle: i16) {
-    println!("アドレス0x{:>02x}のSMDのポート{port}の角度が{angle}度になったとしましょう", address | 0x20);
+fn servo_angle(handle: State<'_, USBHandle>, address: u8, port: u8, angle: i16) {
+    let result = smd::send_angle(
+        &*handle,
+        address | device_type::SMD,
+        port,
+        angle.try_into().unwrap(),
+    )
+    .unwrap();
+    println!("{:?}", result);
 }
 #[tauri::command]
-fn blmd_speed(address: u8, port: u8, speed: i16) {
-    println!("アドレス0x{:>02x}の{port}番ブラシレスモーターが{speed}RPMで回ったとしましょう", address | 0x30);
+fn blmd_speed(handle: State<'_, USBHandle>, address: u8, controller_id: u8, velocity: i16) {
+    let result = blmd::send_velocity(
+        &*handle,
+        address | device_type::BLMD,
+        controller_id,
+        velocity,
+    )
+    .unwrap();
+    println!("{:?}", result);
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let handle = USBHandle::new(0x483, 0x5740, 1);
+            app.manage(handle);
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![motor_pwm, solenoid_state, servo_angle, blmd_speed])
+        .invoke_handler(tauri::generate_handler![
+            motor_pwm,
+            solenoid_state,
+            servo_angle,
+            blmd_speed
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
